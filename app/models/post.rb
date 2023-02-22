06f3e1f9a4f3381c51a171a3b5d2d8a3f53fbb32
Dorothy C <dorothychauu@gmail.com>
2023-02-22 14:31:56 -0500

    commit tree a66d39c64cdc6f3a66749773c5884822e5a5f705

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction','Non-Fiction']}
+    # passing in custom method validations
+    validate :clickbait?
+
+    # defining an array for validations
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    #  custom method for validations
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait") 
+        end
+    end
+
 end
